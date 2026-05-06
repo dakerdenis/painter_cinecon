f80ee5860231c29aa3cbf402f0b35f6e4001c5f2
Denis Akershteyn <dakerdenis@gmail.com>
2026-05-05 23:04:03 -0700

    added main page

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('about-me/', views.about, name='about'),
+    path('work/', views.work, name='work'),
+    path('exhibitions/', views.exhibitions, name='exhibitions'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/pages/about.html')
+
+
+def work(request):
+    return render(request, 'main/pages/work.html')
+
+
+def exhibitions(request):
+    return render(request, 'main/pages/exhibitions.html')
+
+
+def blog(request):
+    return render(request, 'main/pages/blog.html')
+
+
+def contact(request):
+    return render(request, 'main/pages/contact.html')
